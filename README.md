cc01b8776ad5f72af54decaf9526bb4542dc2688
Yannick Uhlmann <augustunderground@protonmail.com>
2023-03-06 18:14:14 +0100

    init

---
+++ README.md
@@ -0,0 +1,28 @@
+# COGNAC
+
+:tumbler_glass: **C**oil **O**peratin**g** Poi**n**t D**a**ta S**c**raper :tumbler_glass: 
+
+Get operating pont data for coils from the coilcraft REST API.
+
+## Usage
+
+1. Adjust the `PartNumber` in `coil.lua`
+1. Run the script
+1. ???
+1. Profit
+
+```lua
+local part = 'XXX'
+```
+
+```shell
+$ lua54 coil.lua
+```
+
+## Dependencies
+
+`Lua` 5.4 and the following rocks:
+
+- `lunajson`
+- `lua-curl`
+- `lanes`
---
+++ coil.lua
@@ -0,0 +1,106 @@
+#!/usr/bin/env lua54
+
+LANES = require 'lanes'
+LINDA = LANES.linda()
+JSON  = require 'lunajson'
+CURL  = require 'cURL'
+
+local adr     = 'https://www.coilcraft.com/api/power-inductor/parts'
+local part    = 'SER2918H-153' -- 'PA6331' 'AGP4233-153'
+
+local outputs = { 'NominalInductance', 'L', 'InductanceAtIpeak', 'Isat', 'IRMS40C'
+                , 'SRFMHz', 'ACLoss', 'DCLoss', 'LossAE', 'LossN', 'LossRFac'
+                , 'LossRth', 'LossVE', 'AdjustedRippleCurrent', 'AdjustedIpeak'
+                , 'PartTemperature', 'Width', 'Length', 'Height', 'LossCore'
+                , 'CoreMaterial', 'Mount', 'Price'}
+
+local inputs  = { 'Temperature', 'Frequency', 'DCcurrent', 'RipplePercent'
+                , 'RippleCurrent', 'PeakCurrent' }
+
+local templateHandle = io.open('./template.json', 'r')
+local template       = JSON.decode(templateHandle:read('*all'))
+_                    = io.close(templateHandle)
+
+local function sendRequest(payload)
+    local curl  = require 'cURL'
+    local json  = require 'lunajson'
+    local res = {}
+    local req = curl.easy{ url        = adr
+                         , post       = true
+                         , httpheader = { "Content-Type: application/json"; }
+                         ; postfields = json.encode(payload);
+                         }
+    req:setopt_writefunction(table.insert, res)
+    local ok,err   = req:perform()
+    local partData = {}
+    print(err)
+    if not err then
+        res = json.decode(table.concat(res))
+        for _,data in pairs(res.PartsData) do
+            if data.PartNumber == part then
+                partData = data
+            end
+        end
+    else
+        print(err)
+    end
+    return partData
+end
+
+local function toCsv(data)
+    local line = ''
+    for _,output in pairs(outputs) do
+        line = line .. ',' .. data[output]
+    end
+    return line
+end
+
+function sendRequestParallel(tmp, frq, idc)
+    print('Sending ' .. tmp .. ',' .. frq .. ',' .. idc)
+    local ric = 0.5
+    local rip = 100 * ric / idc
+    -- local ric = idc * rip / 100;
+    local rpc = ric / 2 + idc;
+    template.current.idcCurrent           = idc
+    template.current.rippleCurrent        = ric
+    template.current.rippleCurrentPercent = rip
+    template.current.ipeakCurrent         = rpc
+    template.frequency.lower              = frq
+    template.frequency.value              = frq
+    template.temperature                  = tmp
+    local xs      = tmp .. ',' .. frq .. ',' .. idc .. ',' .. rip .. ',' .. ric .. ',' .. rpc
+    local res     = sendRequest(template)
+    local csvLine = ''
+    if next(res) ~= nil then
+        local ys  = toCsv(res)
+        csvLine   = xs .. ys .. '\n'
+    end
+    return csvLine
+end
+
+local lines    = {}
+local tmpRange = {-40, -20, 0, 25, 27, 50, 85}
+
+for _,tmp in pairs(tmpRange) do
+    for frqIdx = 1,10 do
+        local frq = frqIdx / 10
+        for idc = 5,10 do
+            -- for rip = 1,10 do
+                local genLine = LANES.gen('*', sendRequestParallel)
+                local csvLine = genLine(tmp,frq,idc)
+                table.insert(lines, csvLine)
+            -- end
+        end
+    end
+end
+
+local csvString = ''
+for _,line in pairs(lines) do
+    csvString = csvString .. line[1]
+end
+
+local csvData   = table.concat(inputs, ',') .. table.concat(outputs, ',')
+                    .. '\n' .. csvString
+local csvHandle = io.open('./loss.csv', 'w')
+_               = csvHandle:write(csvData)
+_               = io.close(csvHandle)
